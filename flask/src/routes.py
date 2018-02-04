81a1ad478ff61b6a9a07a29aea8abc01145d46ff
Vorh <sorcerer12@list.ru>
2018-02-04 21:06:57 +0300

    [ ADD ] config , login form

--- flask/src/routes.py
+++ flask/src/routes.py
@@ -1,10 +1,27 @@
-
 from flask import render_template
 from src import app
+from src.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'YARIK'}
-    return render_template('index.html', user=user)
+
+    posts = [
+        {
+            'author': {'username': 'Arik'},
+            'body': 'Test'
+        },
+        {
+            'author': {'username': 'Test'},
+            'body': 'Max'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
