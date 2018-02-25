d0aaf25424a42d9f221d87cbae0a6d49e4437ed8
Vorh <sorcerer12@list.ru>
2018-02-25 21:53:13 +0300

    [ ADD ] disable cache

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,28 @@
-from flask import Flask, Config
+from flask import Flask, Config, request
 from config import Config
+import os
 import pymysql
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
+@app.url_defaults
+def hashed_static_file(endpoint, values):
+    if 'static' == endpoint or endpoint.endswith('.static'):
+        filename = values.get('filename')
+        if filename:
+            blueprint = request.blueprint
+            if '.' in endpoint:
+                blueprint = endpoint.rsplit('.', 1)[0]
+
+            static_folder = app.static_folder
+            if blueprint and app.blueprints[blueprint].static_folder:
+                static_folder = app.blueprints[blueprint].static_folder
+
+            fp = os.path.join(static_folder, filename)
+            if os.path.exists(fp):
+                values['_'] = int(os.stat(fp).st_mtime)
 
 
 from src import routes
