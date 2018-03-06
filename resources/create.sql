77cd842b7da1a17a035352fc5f75e2af77f6eb6f
Vorh <sorcerer12@list.ru>
2018-03-07 00:18:10 +0300

    [ ADD ]  goal ddl

--- resources/create.sql
+++ resources/create.sql
@@ -7,4 +7,14 @@ CREATE TABLE reward (
   user_id     INT,
 
   PRIMARY KEY (id)
+);
+
+
+CREATE TABLE goal (
+  id          INT AUTO_INCREMENT PRIMARY KEY,
+  caption     VARCHAR(255)                        NULL,
+  create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  user_id     INT                                 NULL,
+  reward      INT                                 NULL,
+  FOREIGN KEY (reward) REFERENCES reward (id)
 )
