08b926d81ddc49f2e8a296469bf22e53072daf9c
Vorh <sorcerer12@list.ru>
2018-03-08 23:56:28 +0300

    [ CHANGE ] logging comments

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ from logging import Formatter
 from src import app
 
 if __name__ == "__main__":
-    # initialize the log handler
     logHandler = logging.StreamHandler()
 
-    # set the log handler level
     logHandler.setLevel(logging.INFO)
 
     logHandler.setFormatter(Formatter('''%(asctime)s %(levelname)s %(module)s - %(message)s'''))
 
-    # set the app logger level
     app.logger.setLevel(logging.INFO)
 
     app.logger.addHandler(logHandler)
